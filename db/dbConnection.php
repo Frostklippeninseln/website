946a287a4d0f09bd8c07c0e9c468ec62f0b9bee2
Maximilian <104452726+eZMasterSin@users.noreply.github.com>
2024-06-10 13:26:55 +0200

    Add files via upload

---
+++ db/dbConnection.php
@@ -0,0 +1,22 @@
+<?php
+    class DBConn {
+
+        private static $servername = "localhost";
+        private static $username = "dbuser";
+        private static $password = "dbpassword";
+        private static $dbname = "Frostklippeninseln";
+
+        private static $conn;
+
+        public static function getConn() {
+            if(!isset(self::$conn)) {
+                self::$conn = new mysqli(self::$servername, self::$username, self::$password, self::$dbname);
+
+                if (self::$conn->connect_error) {
+                    die("Connection failed: " . self::$conn->connect_error);
+                }
+            }
+            return self::$conn;
+        }
+    }
+?>
