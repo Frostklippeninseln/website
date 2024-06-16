9347042d827cede387a0f1bf45260a59b4e8f423
SomerCode <93607321+SomerCode@users.noreply.github.com>
2024-06-17 01:31:10 +0200

    Add files via upload

---
+++ vendor/colinodell/json5/test/Functional/OptionsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the colinodell/json5 package.
+ *
+ * (c) Colin O'Dell <colinodell@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ColinODell\Json5\Test\Functional;
+
+use ColinODell\Json5\Json5Decoder;
+use PHPUnit\Framework\TestCase;
+
+class OptionsTest extends TestCase
+{
+    public function testAssocFalseWithNoOptionsSet()
+    {
+        $result = Json5Decoder::decode('{"foo": true}', false);
+        $this->assertInstanceOf(\stdClass::class, $result);
+    }
+
+    public function testAssocFalseWithAssocOption()
+    {
+        $result = Json5Decoder::decode('{"foo": true}', false, 512, JSON_OBJECT_AS_ARRAY);
+        $this->assertInstanceOf(\stdClass::class, $result);
+    }
+
+    public function testBigIntWithNoOptionsSet()
+    {
+        $result = Json5Decoder::decode('12345678901234567890');
+        $this->assertIsFloat($result);
+    }
+
+    public function testBigIntWithOptionSet()
+    {
+        $result = Json5Decoder::decode('12345678901234567890', false, 512, JSON_BIGINT_AS_STRING);
+        $this->assertIsString($result);
+    }
+}
